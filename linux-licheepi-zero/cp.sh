18809736dbfbaa449f36a063580b8b1d554a9183
Hetao-HUB <402355698@qq.com>
2020-10-30 18:06:04 -0700

    linux

---
+++ linux-licheepi-zero/cp.sh
@@ -0,0 +1,3 @@
+cp arch/arm/boot/zImage /home/zh/linux/tftp/
+cp arch/arm/boot/dts/sun8i-v3s-ht.dtb /home/zh/linux/tftp/
+mv /home/zh/linux/tftp/sun8i-v3s-ht.dtb /home/zh/linux/tftp/sun8i-v3s-licheepi-zero.dtb  
